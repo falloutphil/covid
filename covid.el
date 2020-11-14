72b3071205d3278148a665f53198fb6f53183436
Phil <phil@beadling.co.uk>
2020-11-14 16:31:17 +0000

    change name of function

--- covid.el
+++ covid.el
@@ -250,7 +250,7 @@
   (map-keys covid-country-population-alist))
 
 ;;;###autoload
-(defun covid-country-history (country start-date max-cases)
+(defun covid (country start-date max-cases)
   "Helper function to get covid details from COUNTRY.  If POPULATION is non-zero this is used directly (eg to match ECDC numbers).  START-DATE dictates X-Axis start.  MAX-CASES dictates Y-Axis scale for ASCII plots."
   (interactive (list (ido-completing-read "Country? " covid-country-list)
 		     (org-read-date nil nil nil "Plot Start Date? "
